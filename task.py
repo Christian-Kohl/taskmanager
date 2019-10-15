7a88c82d6ae76755577422ce841467505c786f22
Christian-Kohl <christianalbert.k@gmail.com>
2019-10-15 20:59:36 +0200

    Made basic changes, not close to usable, methods are set up however.

--- task.py
+++ task.py
@@ -1,20 +1,57 @@
 import click
+from tasks import Task
 
 
+@click.group()
+def task():
+    pass
+
+# Command to create a new task, or project
 @click.command()
-@click.option('--project')
-@click.option('--habit', prompt='Is it a habit?', is_flag=True,
-              help='Whether the task is a habit or not.')
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(habit, count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    if habit:
-        click.echo("This is a habit")
-    for x in range(count):
-        click.echo('Hello %s!' % name)
+@click.option('--date', default="today")
+@click.argument('task', nargs=-1)
+def new(date, task):
+    click.echo(date)
+    click.echo(task)
+    # t = Task(date, task)
+    click.echo("Hello!")
 
+
+# Returns the tasks for that day, if one is given, otherwise month, year, or other tasks also work, if not, then a summary is given of all tasks that are to be check_completed
+# Maybe extend this to also work for whole projects, or extend it to work :D
+@click.command()
+def get():
+    click.echo("Gello!")
+
+# Returns a summary for the day, including recommended tasks
+@click.command()
+def summary():
+    click.echo("This is a summary")
+
+
+@click.Command
+@click.argument('task')
+def complete(task):
+    task_list = dict()
+    if task in task_list:
+        task.complete()
+    else:
+        click.echo("That doesn't seem to be a task")
+
+
+@click.Command
+@click.argument('task')
+def delete(task):
+    task_list = dict()
+    if task in task_list:
+        task_list["task"] = None
+    else:
+        click.echo("That doesn't seem to be a task")
+
+
+task.add_command(new)
+task.add_command(get)
+task.add_command(summary)
 
 if __name__ == '__main__':
-    hello()
+    task()
--- tasks.py
+++ tasks.py
@@ -1,10 +1,11 @@
 class Task():
 
-    def __init__(task_date, task_title, task_description):
+    def __init__(task_date, task_title):
         self.date = task_date
         self.title = task_title
-        self.desc = task_desc
+        # self.desc = task_desc
         self.subtasks = []
+        self.completed = False
 
-    def serialize():
-        return serialized_string
+    def check_completed():
+        all([x.completed for x in self.subtasks])
