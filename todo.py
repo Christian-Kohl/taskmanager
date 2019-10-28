9510729c062c3e8d1502b5075915450db9dc2318
Christian-Kohl <christianalbert.k@gmail.com>
2019-10-28 22:20:53 +0100

    Added projects

--- todo.py
+++ todo.py
@@ -13,9 +13,21 @@ def setup():
              (id INTEGER NOT NULL PRIMARY KEY,
              date datetime,
              task text NOT NULL,
+             project_name text NOT NULL,
              habit BIT NOT NULL,
-             increment INTEGER);
+             increment INTEGER,
+             FOREIGN KEY (project_name) references projects(project_name)
+             );
              ''')
+    c.execute('''
+                CREATE TABLE projects
+                (id INTEGER NOT NULL PRIMARY KEY,
+                date datetime,
+                project_name text NOT NULL)
+            ''')
+    c.execute('''
+                INSERT INTO projects (project_name) VALUES(?)
+            ''', ('General', ))
     conn.commit()
     conn.close()
 
@@ -23,8 +35,8 @@ def setup():
 def add_db(task):
     conn = sqlite3.connect("todolist.db")
     c = conn.cursor()
-    c.execute('''INSERT INTO tasks(date, task, habit, increment) VALUES(?, ?, ?, ?)
-             ''', (task.date, task.name, task.habit, task.increment))
+    c.execute('''INSERT INTO tasks(date, task, project_name, habit, increment) VALUES(?, ?, ?, ?, ?)
+             ''', (task.date, task.name, task.proj, task.habit, task.increment))
     conn.commit()
     conn.close()
 
@@ -46,10 +58,12 @@ def todo():
 @click.command()
 @click.option('--date', prompt="Date for todo task")
 @click.option('--todo', prompt="What is your task?")
+@click.option('--project', default='General')
 @click.option('--habit', is_flag=True)
-def add(date, todo, habit):
+def add(date, todo, project, habit):
     date = dateparser.parse(date)
     task = Task(date, todo)
+    task.proj = project
     if habit:
         increment = click.prompt("How often do you want to do your habit?")
         task.habit = 1
@@ -73,9 +87,40 @@ def get():
     print(pd.read_sql('''SELECT * FROM tasks''', con=conn, index_col="id"))
 
 
+@click.command()
+@click.option('--name', prompt="What is your project name?")
+@click.option('--date', default=None)
+def add_proj(name, date):
+    conn = sqlite3.connect("todolist.db")
+    c = conn.cursor()
+    c.execute('''INSERT INTO projects(date, project_name) VALUES(?, ?)
+             ''', (date, name, ))
+    conn.commit()
+    conn.close()
+
+
+@click.command()
+def remove_proj(name):
+    conn = sqlite3.connect("todolist.db")
+    c = conn.cursor()
+    c.execute('''DELETE FROM projects WHERE task = ?;
+             ''', (name, ))
+    conn.commit()
+    conn.close()
+
+
+@click.command()
+def get_proj():
+    conn = sqlite3.connect("todolist.db")
+    print(pd.read_sql('''SELECT * FROM projects''', con=conn, index_col="id"))
+
+
 todo.add_command(add)
 todo.add_command(remove)
+todo.add_command(add_proj)
+todo.add_command(remove_proj)
 todo.add_command(get)
+todo.add_command(get_proj)
 
 
 if __name__ == '__main__':
